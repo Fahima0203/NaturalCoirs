47a42ce55cc5a4d48fcb980ffa10d5c601c9a90b
Fahima0203 <bfahimabegum213@gmail.com>
2025-06-08 18:20:24 +0530

    feat: initialize React app with Bootstrap
    - Added package.json to manage dependencies and scripts
    - Created index.html for the app's main HTML structure
    - Added manifest.json for PWA support
    - Implemented main App component with routing
    - Added Home and About pages
    - Created Navbar component with Bootstrap styling
    - Included custom CSS for Navbar styling
    - Included logo asset for branding

---
+++ src/pages/Navbar.jsx
@@ -0,0 +1,25 @@
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import logo from '../assets/nc_logo.png';
+import '../styles/Navbar.css';
+function Nbar() {
+  return (
+    <Navbar expand="lg" className="my-navbar">
+      <Container>
+        <img src={logo} style={{ width: "70px", borderRadius: "30px", height:"60px", marginRight: "25px" }} alt="" />
+        <Navbar.Brand href="/">Natural Coirs</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="/">Home</Nav.Link>
+            <Nav.Link href="/About">About</Nav.Link>
+            <Nav.Link href="/Contact">Contact</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default Nbar;
