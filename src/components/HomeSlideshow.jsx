2504be81d1fdbfe0bbabc82d8c4f676fa988d714
Fahima0203 <bfahimabegum213@gmail.com>
2025-07-15 21:48:52 +0530

    "add a vedio to homepage"

--- src/components/HomeSlideshow.jsx
+++ src/components/HomeSlideshow.jsx
@@ -1,45 +1,33 @@
 import React from 'react';
 import "../styles/HomeSlideshow.css";
 
-import img1 from '../assets/coco_peat/about_vedio.mp4';
-import img2 from '../assets/coco_peat/Natural Coirs.mp4';
-import img3 from '../assets/coco_peat/image2.png';
-import img4 from '../assets/coco_peat/image2.png';
+import img5 from '../assets/coco_peat/combined_vedio.mp4'
 
-// const colors = [img1, img2, img3, img4]
-const colors = [img4]
-const delay = 3000;
+const colors = [img5];
+const delay = 10000;
 
 function HomeSlideshow() {
     const [index, setIndex] = React.useState(0);    
     const timeoutRef = React.useRef(null);
 
     function resetTimeout() {
-        if (timeoutRef.current) {
-            clearTimeout(timeoutRef.current);
-        }
+        if (timeoutRef.current) clearTimeout(timeoutRef.current);
     }
 
     React.useEffect(() => {
         resetTimeout();
         timeoutRef.current = setTimeout(
-            () =>
-                setIndex((prevIndex) =>
-                    prevIndex === colors.length - 1 ? 0 : prevIndex + 1
-                ),
+            () => setIndex((prevIndex) => (prevIndex === colors.length - 1 ? 0 : prevIndex + 1)),
             delay
         );
-
-        return () => {
-            resetTimeout();
-        };
+        return () => resetTimeout();
     }, [index]);
 
     return (
-        <div className="slideshow" style={{height:'560px'}}>
+        <div className="slideshow-wrapper">
             <div
-                className="slideshowSlider"
-                style={{ transform: `translate3d(${-index * 100}%, 0, 0)`, height: "450px" }}
+                className="slideshow-slider"
+                style={{ transform: `translateX(${-index * 100}%)` }}
             >
                 {colors.map((home, idx) => {
                     const isVideo = typeof home === "string" && home.match(/\.(mp4|webm|ogg)$/i);
@@ -50,6 +38,8 @@ function HomeSlideshow() {
                             src={home}
                             autoPlay
                             loop
+                            muted
+                            playsInline
                         />
                     ) : (
                         <img
@@ -62,14 +52,12 @@ function HomeSlideshow() {
                 })}
             </div>
 
-            <div className="slideshowDots">
+            <div className="slideshow-dots">
                 {colors.map((_, ind) => (
                     <div
                         key={ind}
-                        className={`slideshowDot${index === ind ? " active" : ""}`}
-                        onClick={() => {
-                            setIndex(ind);
-                        }}
+                        className={`dot ${index === ind ? "active" : ""}`}
+                        onClick={() => setIndex(ind)}
                     ></div>
                 ))}
             </div>
